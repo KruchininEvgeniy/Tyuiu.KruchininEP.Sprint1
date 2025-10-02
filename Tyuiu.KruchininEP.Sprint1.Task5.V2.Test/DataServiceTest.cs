aa77366af77b4f7dc2d153863fcdb0a9b0d75969
Евгений Кручинин <Tyuiu.KruchininEP@outlook.com>
2025-10-02 23:13:48 +0500

    Task5

---
+++ Tyuiu.KruchininEP.Sprint1.Task5.V2.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.KruchininEP.Sprint1.Task5.V2.Lib;
+
+namespace Tyuiu.KruchininEP.Sprint1.Task5.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double temp = 50;
+            double res = ds.FahrenheitToСelsius(temp);
+            Assert.AreEqual(10, res);
+        }
+    }
+}
