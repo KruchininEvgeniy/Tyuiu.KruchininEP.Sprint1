42c2a6ae52cda563fdfa8d1eecc96bc6c8218942
Евгений Кручинин <Tyuiu.KruchininEP@outlook.com>
2025-10-02 23:43:12 +0500

    Task6

---
+++ Tyuiu.KruchininEP.Sprint1.Task6.V4.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.KruchininEP.Sprint1.Task6.V4.Lib
+{
+    public class DataService : ISprint1Task6V4
+    {
+        public string CheckDoubleN(string value)
+        {
+            string[] values = value.Split(' ');
+            string res = "";
+            foreach (string s in values)
+            {
+                if (s.Contains("нн")) res += s + " ";
+            }
+            return res.Trim();
+        }
+    }
+}
---
+++ Tyuiu.KruchininEP.Sprint1.Task6.V4.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KruchininEP.Sprint1.Task6.V4.Lib;
+
+namespace Tyuiu.SavtsovNE.Sprint1.Task6.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidString()
+        {
+            DataService ds = new DataService();
+            string strTest = "";
+            string res = ds.CheckDoubleN(strTest);
+            string wait = "";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
